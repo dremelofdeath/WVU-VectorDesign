f8008ec717e0a4d30e56a545b1e4d65e2cc9f356
ben <ben@580bed88-caa2-44de-b298-61e134d9bb2a>
2010-04-13 23:15:37 +0000

    Adding the networking code I have completed thus far. Client-main.cpp and server-main.cpp are designed to test the ClientSocket and ServerSocket classes.

---
+++ server-main.cpp
@@ -0,0 +1,8 @@
+#include "server.h"
+
+int main(int argc, char *argv[])
+{	ServerSocket server;
+
+    server.initServer(argv[1], argv[2]);
+    server.wait();
+}
---
+++ server.h
@@ -0,0 +1,34 @@
+#ifndef __TP_SERVER_SOCKET__
+#define __TP_SERVER_SOCKET__
+
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <string>
+
+using std::string;
+
+int server();
+
+class ServerSocket
+{   public:
+        ServerSocket(int bufLength = 512);
+
+        void initServer(string ip, string port);
+        void wait();
+
+        ~ServerSocket();
+
+    private:
+        void startup();
+        void addressInfo(string ip, string port);
+        void createSocket();
+        void bindSocket();
+
+        WSADATA wsaData;
+        struct addrinfo *result;
+        struct addrinfo *ptr;
+        struct addrinfo hints;
+        SOCKET ListenSocket;
+};
+
+#endif
