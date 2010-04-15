17b960fc2a8711ae01cedeb3d92b044a4debff40
ben <ben@580bed88-caa2-44de-b298-61e134d9bb2a>
2010-04-15 23:22:51 +0000

    Actually added the definition to Orientation.h.  My bad...

--- Orientation.h
+++ Orientation.h
@@ -1,2 +1,20 @@
+#include <GL/gl.h>
+
 #include "Renderable.h"
 
+class Orientation : public Renderable
+{   public:
+        Orientation();
+
+        int loadTexture_Ipl(IplImage *image, GLuint *text);
+        int fillRotationArray(double x, double y, double z, double angle, double rotArr[9]);
+        void myReshape(int w, int h);
+        void render(void);
+        void idle();
+        void CreateGlutCallbacks();
+        void initGL();
+
+        int main(int argc, char * const argv[]);
+
+        ~Orientation();
+};
