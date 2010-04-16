dc6e9b4422101a0747833fc637c98a6d75e5e8bf
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2010-04-16 00:28:46 +0000

    Updating handlers and the Orientation class

--- Orientation.h
+++ Orientation.h
@@ -11,22 +11,17 @@
 #include "Renderable.h"
 
 class Orientation : public Renderable
-{   public:
-        Orientation();
-
-		GLuint frameTex;
-
-        int loadTexture_Ipl(IplImage *image, GLuint *text);
-        int fillRotationArray(double x, double y, double z, double angle, double rotArr[9]);
-        void myReshape(int w, int h);
-        void render(void) const;
-        void idle();
-        void CreateGlutCallbacks();
-        void initGL();
-
-        int main(int argc, char * const argv[]);
-
-        ~Orientation();
+{
+  public:
+    Orientation();
+    int loadTexture_Ipl(IplImage *image, GLuint *text);
+    int fillRotationArray(double x, double y, double z, double angle, double rotArr[9]);
+    void render(void) const;
+    void idle();
+    ~Orientation();
+  private:
+    CvHaarClassifierCascade* _cascade;
+		GLuint _frameTex;
 };
 
 #endif
