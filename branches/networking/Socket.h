c10e30e5c68e1cc54105e568963fb33b2a9f2ed9
ben <ben@580bed88-caa2-44de-b298-61e134d9bb2a>
2010-04-19 08:42:16 +0000

    Cleaned the interface more and added functionality for the ServerSocket class. The TCP networking code is essentially complete - it still needs tested.

--- branches/networking/Socket.h
+++ branches/networking/Socket.h
@@ -12,16 +12,23 @@ using std::string;
 class Socket
 { public:
     Socket() throw(NetworkException);
-    Socket(string ip, string port) throw(NetworkException);
+    Socket(string const ip, string const port) throw(NetworkException);
 
-    virtual void connectSocket(string ip, string port);
+    virtual void connectTo(string const ip, string const port);
+    virtual void connectSocket() = 0;
     virtual int sendData(char const * const data, int length) = 0;
     virtual int receiveData(char * const data, int length) = 0;
     virtual void disconnect() = 0;
 
   protected:
-    string ip;
-    string port;
+    virtual void setIp(string const ip);
+    virtual void setPort(string const port);
+    virtual const string getIp();
+    virtual const string getPort();
+
+  private:
+    string _ip;
+    string _port;
 
     WSADATA wsaData;
 };
--- branches/networking/TCPSocket.cpp
+++ branches/networking/TCPSocket.cpp
@@ -1,56 +1,51 @@
 #include "TCPSocket.h"
 
 TCPSocket::TCPSocket() : Socket()
-{
+{ setSocket(INVALID_SOCKET);
 }
 
 TCPSocket::TCPSocket(string ip, string port) : Socket(ip, port)
-{
+{ setSocket(INVALID_SOCKET);
 }
 
-void TCPSocket::connectSocket(string ip, string port)
-{ // This is very bad to use. It zeros the memory pointed to. We need a platform
-  // independent solution, as this likely only works for Windows.
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
+void TCPSocket::connectSocket()
+{ ZeroMemory(&_hints, sizeof(_hints));
+	_hints.ai_family = AF_UNSPEC;
+	_hints.ai_socktype = SOCK_STREAM;
+	_hints.ai_protocol = IPPROTO_TCP;
 
-  result = NULL;
-	int error = getaddrinfo(ip.c_str(), port.c_str(), &hints, &result);
+  _result = NULL;
+	int error = getaddrinfo(getIp().c_str(), getPort().c_str(), &_hints, &_result);
 	if(error != 0)
 	{	string str= "getaddrinfo failed: "  + error;
 		WSACleanup();
 		throw NetworkException(str);
 	}
 
-  sock = INVALID_SOCKET;
-	ptr = result;
-	sock = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-	if(sock == INVALID_SOCKET)
+	_ptr = _result;
+	setSocket(socket(_ptr->ai_family, _ptr->ai_socktype, _ptr->ai_protocol));
+	if(getSocket() == INVALID_SOCKET)
 	{	string str = "Error at socket(): " + WSAGetLastError();
-		freeaddrinfo(result);
+		freeaddrinfo(_result);
 		WSACleanup();
 		throw NetworkException();
 	}
 
-  error = connect(sock, ptr->ai_addr, (int)ptr->ai_addrlen);
+  error = connect(getSocket(), _ptr->ai_addr, (int)_ptr->ai_addrlen);
   if(error == SOCKET_ERROR)
-  {   closesocket(sock);
-      sock = INVALID_SOCKET;
+  {   closesocket(getSocket());
+      setSocket(INVALID_SOCKET);
   }
   
-  freeaddrinfo(result);
-  if(sock == INVALID_SOCKET)
+  freeaddrinfo(_result);
+  if(getSocket() == INVALID_SOCKET)
   {   WSACleanup();
       throw NetworkException("Unable to connect to server.");
   }
-
-  connectSocket();
 }
 
 int TCPSocket::sendData(char const * const data, int length)
-{ int bytes = send(sock, data, length, 0);
+{ int bytes = send(getSocket(), data, length, 0);
   if(bytes == SOCKET_ERROR)
   { string str = "Send failed: " + WSAGetLastError();
     throw NetworkException(str);
@@ -59,7 +54,7 @@ int TCPSocket::sendData(char const * const data, int length)
 
 int TCPSocket::receiveData(char * const data, int length)
 { int bytes = 0;
-  bytes = recv(sock, data, length, 0);
+  bytes = recv(getSocket(), data, length, 0);
   if(bytes < 0)
   { string str = "Receive failed: " + WSAGetLastError();
     throw NetworkException(str);
@@ -67,12 +62,26 @@ int TCPSocket::receiveData(char * const data, int length)
 }
 
 void TCPSocket::disconnect()
-{ int error = shutdown(sock, SD_SEND);
+{ int error = shutdown(getSocket(), SD_SEND);
   if(error == SOCKET_ERROR)
   { string str = "Shutdown failed: " + WSAGetLastError();
-    closesocket(sock);
+    closesocket(getSocket());
     throw NetworkException(str);
   }
 }
 
+void TCPSocket::setWsaData(WSADATA const wsaData)
+{ _wsaData = wsaData;
+}
 
+void TCPSocket::setSocket(SOCKET const sock)
+{ _sock = sock;
+}
+
+const WSADATA TCPSocket::getWsaData()
+{ return _wsaData;
+}
+
+const SOCKET TCPSocket::getSocket()
+{ return _sock;
+}
