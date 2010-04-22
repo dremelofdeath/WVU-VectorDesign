90c9fac5c5dcf1a7433ec6254665e221fa0e83e0
mike <mike@580bed88-caa2-44de-b298-61e134d9bb2a>
2010-04-22 08:20:32 +0000

    TCPSocket.h documented, kinda...

--- TCPSocket.h
+++ TCPSocket.h
@@ -1,3 +1,7 @@
+/*!	\file TCPSocket.h
+ *	\author Ben Roberts
+ */
+
 #ifndef __TCP_SOCKET_H__
 #define __TCP_SOCKET_H__
 
@@ -28,3 +32,50 @@ class TCPSocket : public Socket
 };
 
 #endif
+
+/*!	\class TCPSocket
+ *	\brief ....
+ *
+ *	...detailed...
+ */
+
+/*!	\fn TCPSocket(string const ip, string const port)
+ *	\brief Constructor
+ *	\param ip
+ *	  The desired destination ip address.
+ *	\param port
+ *	  The desired port to establish the connection over.
+ *	\memberof TCPSocket
+ */
+
+/*!	\fn virtual void connectSocket()
+ *	\brief Attempts to establish a TCP connection with the server.
+ *	\memberof TCPSocket
+ */
+
+/*!	\fn virtual int sendData(char const * const data, int length)
+ *	\brief Sends data over the TCP connection.
+ *	\param data
+ *	  The data that will be sent.
+ *	\param length
+ *	  The length of the data being sent.
+ *	\return
+ *	  .............
+ *	\memberof TCPSocket
+ */
+
+/*!	\fn virtual int receiveData(char * const data, int length)
+ *	\brief Recieves data through the TCP connection.
+ *	\param data
+ *	  The storage place for the incoming data.
+ *	\param length
+ *	  The expected length of the incoming data.
+ *	\return
+ *	  .......................
+ *	\memberof TCPSocket
+ */
+
+/*!	\fn virtual void disconnect()
+ *	\brief Closes the socket and disconnects the TCP connection.
+ *	\memberof TCPSocket
+ */
